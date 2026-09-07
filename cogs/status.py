69db309a1d3440051a20c82c1f3282bd1d1a611e
Hano2001 <haraldnorlin93@gmail.com>
2026-09-07 13:40:52 +0200

    sqlite connection and first query

--- cogs/status.py
+++ cogs/status.py
@@ -1,47 +1,52 @@
 import discord
 from discord.ext import commands
-from data import sensor_data
+#from data import sensor_data
 import modules
+import sqlite3
 
 class Status(commands.Cog):
+    
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command()
     async def sensors(self, ctx:commands.Context):
-       
-        for sensor in sensor_data:
+        print("Trying to see list of sensors")
+        db_connection = sqlite3.connect("./storage/database.db")
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT * FROM SENSORS")
+        for row in cursor.fetchall():
+                print(row)
+                await ctx.send(row)
 
-            await ctx.send(sensor)
-
-
-    @commands.command()
-    async def check(self,ctx:commands.Context, arg):
-        for sensor in sensor_data:
-            await ctx.send({"id" : sensor["id"], "name" : sensor["name"], arg:sensor[arg]})
-
-
-    @commands.command()
-    async def deletesensor(self,ctx:commands.Context, arg):
-        for sensor in sensor_data:
-            if sensor.get("id") == arg:
-                name = sensor["name"]
-                sensor_data.remove(sensor)
-                await ctx.send(f"Sensor {name} was succesfully removed!")
-                break;
-
-    @commands.command()
-    async def status(self,ctx:commands.Context,arg = ""):
-        for sensor in sensor_data:
-            if arg == "":
-                status = modules.field_status_check(sensor)
-                await ctx.send(sensor)   
-                await ctx.send(status if status != [] else "Status OK")   
-            else:
-                if sensor.get("id") == arg:
-                    status = modules.field_status_check(sensor)
-                    await ctx.send(status if status != [] else "Status OK")
+        db_connection.close()
+    # @commands.command()
+    # async def check(self,ctx:commands.Context, arg, arg2):
+    #     for sensor in sensor_data:
+    #         await ctx.send({"id" : sensor["id"], "name" : sensor["name"], arg:sensor[arg]})
 
 
+    # @commands.command()
+    # async def deletesensor(self,ctx:commands.Context, arg):
+    #     for sensor in sensor_data:
+    #         if sensor.get("id") == arg:
+    #             name = sensor["name"]
+    #             sensor_data.remove(sensor)
+    #             await ctx.send(f"Sensor {name} was succesfully removed!")
+    #             break;
+
+    # @commands.command()
+    # async def status(self,ctx:commands.Context,arg = ""):
+    #     for sensor in sensor_data:
+    #         if arg == "":
+    #             status = modules.field_status_check(sensor)
+    #             await ctx.send(sensor)   
+    #             await ctx.send(status if status != [] else "Status OK")   
+    #         else:
+    #             if sensor.get("id") == arg:
+    #                 status = modules.field_status_check(sensor)
+    #                 await ctx.send(status if status != [] else "Status OK")
+
+    
 async def setup(bot:commands.Bot):
     await bot.add_cog(Status(bot))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-import json
+import sqlite3
 import asyncio
 import modules
 
@@ -11,6 +11,21 @@ token = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 intents = discord.Intents.default()
 intents.message_content = True
+
+db_connection = sqlite3.connect("./storage/database.db")
+cursor = db_connection.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS SENSORS(NAME VARCHAR(255), TEMP REAL, VIBRATION REAL, PRESSURE REAL)""")
+cursor.execute("INSERT INTO SENSORS VALUES ('Main Compressor Intake',42.5,1.2,101.3 )")
+
+print("Data Inserted in the table: ")
+cursor.execute("SELECT * FROM SENSORS")
+for row in cursor.fetchall():
+    print(row)
+
+# Commit changes and close connection
+db_connection.commit()
+db_connection.close()
 
 async def load_extensions():
     for filename in os.listdir('./cogs'):
